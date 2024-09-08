b0d2bc2ec4efb66b15f697952dcd09f566f91fe9
lcsin <1847>
2024-09-08 12:10:21 +0800

    new

--- judge/judge.go
+++ judge/judge.go
@@ -1,6 +1,8 @@
 package judge
 
-import "regexp"
+import (
+	"regexp"
+)
 
 // Number 判断文本是否为纯数字
 func Number(text string) bool {
@@ -18,4 +20,39 @@ func ContainChinese(text string) bool {
 	}
 	matched, _ := regexp.MatchString("\\p{Han}", text)
 	return matched
+}
+
+// BalancedPlaceholder 判断文本中的占位符对是否左右匹配
+func BalancedPlaceholder(text string, dict map[string]string) bool {
+	if len(text) == 0 {
+		return false
+	}
+
+	stack := make([]string, 0, len(text))
+	left := make(map[string]bool, len(dict)/2)
+	right := make(map[string]bool, len(dict)/2)
+	reverse := make(map[string]string, len(dict))
+	for k, v := range dict {
+		reverse[v] = k
+	}
+
+	for k, v := range dict {
+		left[k] = true
+		right[v] = true
+	}
+
+	for _, v := range text {
+		str := string(v)
+		switch true {
+		case left[str]: // 左边直接入栈
+			stack = append(stack, str)
+		case right[str]: // 右边判断是否匹配
+			if len(stack) == 0 || stack[len(stack)-1] != reverse[str] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
 }
---
+++ judge/judge_test.go
@@ -0,0 +1,22 @@
+package judge
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBalancedHolder(t *testing.T) {
+	dict := make(map[string]string, 0)
+	dict["{"] = "}"
+	dict["["] = "]"
+	dict["<"] = ">"
+
+	text := "a{nihao}b,c[nihao]d,<nihao>.aaa"
+	fmt.Println(BalancedPlaceholder(text, dict))
+
+	text = "a{nihao},b{nihao},c[<sdfsf>]"
+	fmt.Println(BalancedPlaceholder(text, dict))
+
+	text = "{<>[]<}}"
+	fmt.Println(BalancedPlaceholder(text, dict))
+}
