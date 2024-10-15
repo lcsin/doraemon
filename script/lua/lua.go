d7c088f742911f02f3ff22cbec32b5b3553d9d92
lcsin <1847@qq.com>
2024-10-15 11:36:42 +0800

    new

--- script/lua/lua.go
+++ script/lua/lua.go
@@ -3,13 +3,16 @@ package lua
 import _ "embed" // embed lua script
 
 // RedisDistributeLock Redis分布式锁脚本-获取锁
+//
 //go:embed redis_distribute_lock.lua
 var RedisDistributeLock string
 
 // RedisDistributeUnLock Redis分布式锁脚本-释放锁
+//
 //go:embed redis_distribute_unlock.lua
 var RedisDistributeUnLock string
 
-// RedisSlideWindowRatelimiter Redis基于滑动窗口算法的限流
+// RedisSlideWindowRateLimiter Redis基于滑动窗口算法的限流
+//
 //go:embed slide_window_limiter.lua
-var RedisSlideWindowRatelimiter string
+var RedisSlideWindowRateLimiter string
