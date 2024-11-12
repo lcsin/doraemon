8ec1a4bcaaf91fbfdbb83c1f51cd732eb24ac663
lcsin <1847@qq.com>
2024-11-12 11:08:33 +0800

    gormx

---
+++ gormx/query.go
@@ -0,0 +1,27 @@
+package gormx
+
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+// Upset 插入或更新
+//
+//	@param db
+//	@return error
+func Upset(db *gorm.DB, updates map[string]any, create any) error {
+	return db.Clauses(clause.OnConflict{
+		DoUpdates: clause.Assignments(updates),
+	}).Create(create).Error
+}
+
+// Paginate 分页
+//
+//	@param db
+//	@return error
+func Paginate(db *gorm.DB, pageNo, pageSize int64) *gorm.DB {
+	if pageNo > 0 && pageSize > 0 {
+		return db.Limit(int(pageSize)).Offset(int((pageNo - 1) * pageSize))
+	}
+	return db
+}
